50bdcc5043842cfe30ab0265d1dd456ef9bca6ce
garfbit <104676660+garfbit@users.noreply.github.com>
2022-09-11 07:16:27 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 let numero1 = 0
 let numero2 = 0
+let suma = 0
 basic.forever(function () {
-    numero1 = 0
-    numero2 = 0
-    basic.showNumber(numero1 + numero2)
+    numero1 = 5
+    numero2 = 56
+    suma = numero1 + numero2
+    basic.showNumber(suma)
 })
